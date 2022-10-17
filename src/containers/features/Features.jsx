fc2438ddeb3452f4e0004d323c3c69734e84afd2
mbohbless <mbohblesspearl@gmail.com>
2022-10-17 23:27:40 +0100

    the future is not

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import { Feature } from '../../components'
 import './features.css'
 
+const featuresData= [
+  {
+    title:"Improving end distrusts instantly",
+     text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, dignissimos! Id, nihil. Tempore repellendus in sed sapiente, aspernatur totam doloremque? Voluptas voluptatum expedita dolore quidem, consequatur velit quam molestias soluta."
+  },
+  {
+    title:"Become the tended active",
+     text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, dignissimos! Id, nihil. Tempore repellendus in sed sapiente, aspernatur totam doloremque? Voluptas voluptatum expedita dolore quidem, consequatur velit quam molestias soluta."
+  },
+  {
+    title:"Message or am noticing",
+     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, dignissimos! Id, nihil. Tempore repellendus in sed sapiente, aspernatur totam doloremque? Voluptas voluptatum expedita dolore quidem, consequatur velit quam molestias soluta."
+  },
+  {
+    title:"Really boy law county",
+     text: "  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, dignissimos! Id, nihil. Tempore repellendus in sed sapiente, aspernatur totam doloremque? Voluptas voluptatum expedita dolore quidem, consequatur velit quam molestias soluta."
+  },
+]
 const Features = () => {
+
   return (
-    <div>Features</div>
+    <div className='gpt3__features section__padding' id="features">
+      <div className='gpt3__features-heading'>
+      <h1 className='gradient__text'>
+        The Future is Now You Just need To Realize it. Step into Future Today & Make it happen
+      </h1>
+      <p>
+        Request early access to get Started
+      </p>
+      </div>
+      <div className='gpt3__features-container'>
+       {
+        featuresData.map((feature, index) => (
+          <Feature key={index} {...feature} />
+        ))
+       }
+      </div>
+    </div>
   )
 }
 
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,20 +6,19 @@ const WhatGPT3 = () => {
   return (
     <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
       <div className="gpt3__whatgpt3__feature">
-        <Feature/>
+        <Feature title="What is GPT-3" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo consequatur, eaque quaerat rem laborum laudantium adipisci nisi reiciendis reprehenderit! Aperiam quod enim, quas omnis aut dolore earum reprehenderit? Atque, praesentium."/>
         <div className='gpt3__whatgpt3-heading'>
           <h1 className='gradient__text'>The Possibilities are beyond your imagination</h1>
           <p>Explore the library</p>
         </div>
         <div className='gpt3__whatgpt3-container'>
-          <Feature/>
-          <Feature/>
-          <Feature/>
-
-
+          <Feature title= "Chatbots" text="We so opinion friends me message as delight whole front do not heard oh"/>
+          <Feature title="Knowledgebase" text = "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam doloribus doloremque amet beatae."/>
+          <Feature title="Education" text=" Veritatis ipsam nihil aspernatur eos, soluta amet quam provident ratione molestias non officia repudiandae asperiores itaque, dignissimos a nam enim, ea vero."/>
         </div>
 
       </div>
+
     </div>
   )
 }
