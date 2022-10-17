68ab103dc08cb0815a06ee6253ebd39cebd65e7e
mbohbless <mbohblesspearl@gmail.com>
2022-10-18 00:06:41 +0100

    possibilities section

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <>
     <p><a href='#home'>Home</a></p>
     <p><a href='#wgpt3'>What is GPT3</a></p>
-    <p><a href='#posibility'>OpenAI</a></p>
+    <p><a href='#possibility'>OpenAI</a></p>
     <p><a href='#features'>Case Studies</a></p>
     <p><a href='#blog'>Library</a></p>
     </>
--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import './possibility.css'
-
+import possibliltyImage from '../../assets/possibility.png';
+ 
 const Possibility = () => {
   return (
-    <div>Possbility</div>
+    <div className='gpt3_possibility section__padding' id="possibility">
+      <div className='gpt3__possiblity-image'>
+        <img src={possibliltyImage}/>
+      </div>
+      <div className='gpt3__possibility-content'>
+       <h4> Request Early access to get started</h4>
+       <h1 className='gradient__text'> The possibilities are beyond our imagination </h1>
+       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore eligendi eaque, repudiandae atque officia minus. Similique, facilis beatae. Ipsa consequatur corporis earum ab perspiciatis asperiores sunt quasi at fugiat totam.</p>
+       <h4>Request Early access to get sterted</h4>
+      </div>
+    </div>
   )
 }
 
