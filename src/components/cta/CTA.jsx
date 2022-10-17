cb706e0dce510b5d7636635f7fa2db9b5eae8e33
mbohbless <mbohblesspearl@gmail.com>
2022-10-18 00:27:49 +0100

    early access

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,17 @@ import './cta.css'
 
 const CTA = () => {
   return (
-    <div>Cta</div>
+    <div className='gpt3__cta section__margin'>
+    <div className='gpt3__cta-content'>
+      <p>Request Early Access</p>
+      <h3>
+        Reister today & start exploring the endless possibilities
+      </h3>
+    </div>
+    <div className='gpt3__cta-button'>
+    <button type='button'>Get Started</button>
+    </div>
+    </div>
   )
 }
 
